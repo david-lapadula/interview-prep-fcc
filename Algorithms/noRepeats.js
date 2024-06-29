1e1b9cdcfa5e8d8c8a07fdfe2abcd40b53e5b0df
DavidLapadula <lapadula.david@gmail.com>
2024-06-28 22:56:30 -0400

    [Algorithms] No repeat in string permutations done. Need to add comments and optimize

--- Algorithms/noRepeats.js
+++ Algorithms/noRepeats.js
@@ -1,12 +1,12 @@
-function permAlone(str) {
-    if (str.length === 0) return ['']; // base call will be reached for each letter in the string
+function getPermutations(str) {
+    if (str.length === 0) return ['']; 
 
     let permutations = [];
 
     for (let i = 0; i < str.length; i++) {
         let char = str[i];
-        let remainingString = str.slice(0, i) + str.slice(i + 1); // get rid of current character. slice(x, y) will keep everything before but not include index of y, slice(z) wil remove z numbers from the front
-        let remainingPermutations = permAlone(remainingString);
+        let remainingString = str.slice(0, i) + str.slice(i + 1);
+        let remainingPermutations = getPermutations(remainingString);
 
         for (let perm of remainingPermutations) {
           permutations.push(char + perm)
@@ -14,7 +14,43 @@ function permAlone(str) {
     }
 
     return permutations;
-  }
+}
+
+
+function permAlone(str) {
+  let total = 0; 
+    function getPermutations(str) {
+      if (str.length === 0) return ['']; 
+
+      let permutations = [];
+
+      for (let i = 0; i < str.length; i++) {
+          let char = str[i];
+          let remainingString = str.slice(0, i) + str.slice(i + 1);
+          let remainingPermutations = getPermutations(remainingString);
+
+          for (let perm of remainingPermutations) {
+            permutations.push(char + perm)
+          }
+      }
+
+      return permutations;
+    }
+
+    let permutations = getPermutations(str);
+
+    for (let perm of permutations) {
+      let hasDuplicate = false;
+      for (let i = 0; i < perm.length - 1; i++) {
+          if (perm[i] === perm[i + 1]) hasDuplicate = true;
+      }
+      
+      if (!hasDuplicate) total++;
+    }
+
+    return total; 
+
+}
   
   
-console.log(permAlone('abc'));
+console.log(permAlone('aab'));
