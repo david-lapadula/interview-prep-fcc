68be84d7b30986bc878e2cdf7e3129c502ee6a05
DavidLapadula <lapadula.david@gmail.com>
2024-09-23 21:13:15 -0400

    [DataStructures] complete bfs graph

--- DataStructures/graph.js
+++ DataStructures/graph.js
@@ -27,8 +27,62 @@
                 [1, 0],
                 [1, 1]
             ];
-    - Traversal: visit nodes in the graph
+    - Traversal: visit nodes in the graph. Check if value is there or find distance
         - BFS
+            - Queue or FIFO data structure. Do this to explore first neighbour next.
+            - Start count with large number like Infinity to capture cases when node may not be reachable from start node
+            - Shortest path, cycle detection
+            - O (V + E)
+            - Strategy
+                - Infinity for all keys which is used to track unvisited nodes
+                - Add root as first element queue.
+                - Get the first element of the queue, and then the layer for the root which is all neighbors 1 edge away
+                - Get the indices of all the neighbours
+                - For each index, which is a neighbor, if it has not been visited
+                    - Push it into queue
+                    - Add 1 to whatever the current distance is from the root for the current item, and make that the distance at the index of the neighbour
+                - Works because each layer has indices of neighbours, and those indices match the keys of the object returned
         - DFS
 */
 
+
+var exBFSGraph = [
+    [0, 1, 0, 0],
+    [1, 0, 1, 0],
+    [0, 1, 0, 1],
+    [0, 0, 1, 0]
+];
+
+function bfs(graph, root) {
+    var nodesLen = {};
+    for (var i = 0; i < graph.length; i++) {
+        nodesLen[i] = i === root ? 0 : Infinity;
+    }
+
+    let queue = [root];
+    let current; 
+
+    while (queue.length !== 0) {
+        current = queue.shift();
+
+        let currConnected = graph[current];
+        let neighborIdx = [];
+        let idx = currConnected.indexOf(1);
+
+        while (idx !== -1) {
+            neighborIdx.push(idx);
+            idx = currConnected.indexOf(1, idx + 1);
+        }
+
+        for (let j = 0; j < neighborIdx.length; j++) {
+            if (nodesLen[neighborIdx[j]] === Infinity) {
+                nodesLen[neighborIdx[j]] = nodesLen[current] + 1;
+                queue.push(neighborIdx[j])
+            }
+        }
+    }
+
+    return nodesLen;
+};
+
+console.log(bfs(exBFSGraph, 1));
