fb372bfc4e7c90f1b0d5551b170457ccd384ef74
DavidLapadula <lapadula.david@gmail.com>
2024-10-23 21:39:34 -0400

    [Projects][game-of-life] init new project

---
+++ Projects/game-of-life/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div style={{ width: '100%' }}>
+      <h1
+        style={{ color: '#66ff33', fontFamily: 'Montserrat', textAlign: 'center' }}>
+        Conway's Game of Life
+      </h1>
+    </div>
+  );
+}
+
+export default App;
