9c178f17daf75bfb44314d685e3cde1747661d69
DavidLapadula <lapadula.david@gmail.com>
2024-11-18 21:05:42 -0500

    [Projects][game-of-life] complete game

--- Projects/game-of-life/src/App.js
+++ Projects/game-of-life/src/App.js
@@ -1,38 +1,91 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Cell from './Cell';
 import './App.css';
 
 function App() {
+
+  const createRandomArray = (rows, columns) => {
+    return Array
+      .from({ length: rows }, () => Array(columns).fill(0))
+      .map(row =>
+        row.map(() => Math.round(Math.random()))
+      );
+  }
+
   const ACTIVE = 'active';
   const PAUSED = 'paused';
-  const [cellArray, setCellArray] = useState(Array.from({ length: 20 }, () => Array(50).fill(0)));
+  const NEW = 'new';
+  const [cellArray, setCellArray] = useState(createRandomArray(20, 50));
   const [gameState, setGameState] = useState(ACTIVE);
   const [generations, setGenerations] = useState(0);
 
-  const startPauseResume = () => {
-    console.log('startPauseResume');
-  }
 
-  const randomize = () => {
-    console.log('randomize');
-  }
+  useEffect(() => {
+    const interval = setInterval(() => {
 
-  const clearBoard = () => {
-    console.log('clearBoard');
-  }
+      setCellArray((prevArray) => {
+        let deepCopyArray = prevArray.map(row => row.slice());
+        let nextGeneration = createNextGeneration(deepCopyArray);
 
-  const toggleCellState = (rowIndex, colIndex) => {
-    console.log('toggle')
-    const newCellArray = cellArray.map((row, rIndex) =>
-      row.map((cell, cIndex) => {
-        if (rIndex === rowIndex && cIndex === colIndex) {
-          return cell === 1 ? 0 : 1;
+        if (gameState === ACTIVE) {
+          setGenerations((prevGen) => {
+            return prevGen + 1;
+          })
+          return nextGeneration;
         }
-        return cell;
+
+        return deepCopyArray;
+      });
+
+    }, 100);
+
+    return () => clearInterval(interval);
+  }, [gameState]);
+
+
+  const createNextGeneration = (currentArray) => {
+    return currentArray.map((row, rowIndex) =>
+      row.map((col, colIndex) => {
+        let rowM1 = rowIndex - 1,
+          colM1 = colIndex - 1,
+          rowP1 = rowIndex + 1,
+          colP1 = colIndex + 1,
+          sum = 0,
+          cell = currentArray[rowIndex][colIndex],
+          neighbours = [
+            [rowM1, colM1], [rowM1, colIndex], [rowM1, colP1], [rowIndex, colP1], [rowP1, colP1], [rowP1, colIndex], [rowP1, colM1], [rowIndex, colM1]
+          ];
+
+        for (let neighbour of neighbours) {
+          let row = neighbour[0],
+            col = neighbour[1];
+
+          if (row >= 0 && col >= 0 && row < currentArray.length && col < currentArray[row].length) {
+            sum += currentArray[row][col];
+          }
+        }
+
+        if (cell === 1) {
+          if (sum < 2) return 0;
+          if (sum === 2 || sum === 3) return 1;
+          if (sum > 3) return 0;
+        } else {
+          if (sum === 3) {
+            return 1;
+          }
+          return 0;
+        }
       })
     );
-    setCellArray(newCellArray);
-  };
+  }
+
+  const startPauseResume = () => {
+    if (gameState === ACTIVE) {
+      setGameState(PAUSED);
+    } else {
+      setGameState(ACTIVE);
+    }
+  }
 
   return (
     <div className="container">
@@ -51,8 +104,6 @@ function App() {
           Generation: {generations}
         </div>
         <div className='game-setting game-font' onClick={startPauseResume}>Start/Pause/Resume</div>
-        <div className='game-setting game-font' onClick={randomize}>Randomize</div>
-        <div className='game-setting game-font' onClick={clearBoard}>Clear board</div>
       </div>
     </div>
   );
